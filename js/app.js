eaf55b215b6e2ac883bceb3afd6526c54f98ed08
Camilo Alexander Amezquita Cortes <111710144+DevCatX@users.noreply.github.com>
2025-08-20 10:49:58 -0500

    Agregadas validaciones del boton sortearAmigos

--- js/app.js
+++ js/app.js
@@ -22,15 +22,22 @@ function agregarAmigo(){
     txtAmigo.value = "";
     ulListaAmigos.appendChild(nuevoLi); // a la lista html se le añade el elenemto li como elemeto hijo
     console.log(listaAmigos);
+    //btnSortearAmigo.removeAttribute("disabled")
   }
 }
 btnAgregarAmigo.addEventListener("click", agregarAmigo);
 
 function sortearAmigo(){
-  let sorteo = numeroRandom(listaAmigos.length); // se ramdomisa un numero entre 1 y el largo de la lista
-  let nuevoLi = document.createElement("li");
-  ulListaAmigos.style.display = "none"; // oculta la vista de la lista con los nombres
-  nuevoLi.innerHTML = `Tu amigo secreto es: ${listaAmigos[sorteo - 1]}`; // se imprime el valor del numero ramdom como indice de la lista, -1 ya que los indices arrancan desde 0 y no desde 1
-  ulResultado.appendChild(nuevoLi);
+  if (listaAmigos.length < 3){
+    alert("Debes ingresar al menos 3 amigos para poder realizar el sorteo");
+  }
+  else{
+    let sorteo = numeroRandom(listaAmigos.length); // se ramdomisa un numero entre 1 y el largo de la lista
+    let nuevoLi = document.createElement("li");
+    ulListaAmigos.style.display = "none"; // oculta la vista de la lista con los nombres
+    nuevoLi.innerHTML = `Tu amigo secreto es: ${listaAmigos[sorteo - 1]}`; // se imprime el valor del numero ramdom como indice de la lista, -1 ya que los indices arrancan desde 0 y no desde 1
+    ulResultado.appendChild(nuevoLi);
+    btnSortearAmigo.setAttribute("disabled", "");// cuando se juega el sorteo el boton se bloquea
+  }
 }
 btnSortearAmigo.addEventListener("click", sortearAmigo);
